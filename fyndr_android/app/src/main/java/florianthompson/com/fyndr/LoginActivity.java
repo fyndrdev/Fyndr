914659a73443559f30bd63dd12f83a773afa0f7f
Florian Thompson <florian.thompson@web.de>
2015-07-23 09:22:18 +0200

    Fix compiling error

--- fyndr_android/app/src/main/java/florianthompson/com/fyndr/LoginActivity.java
+++ fyndr_android/app/src/main/java/florianthompson/com/fyndr/LoginActivity.java
@@ -1,9 +1,11 @@
 package florianthompson.com.fyndr;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.TextView;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -11,7 +13,8 @@ import butterknife.OnClick;
 
 public class LoginActivity extends AppCompatActivity {
 
-@InjectView(R.id.btnSignIn) Button mBtnSignIn;
+    @InjectView(R.id.btnSignIn) Button mBtnSignIn;
+    @InjectView(R.id.textSignUp) TextView mSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,5 +31,11 @@ public class LoginActivity extends AppCompatActivity {
     @OnClick(R.id.btnSignIn)
     public void signIn(){
         finish();
+    }
+
+    @OnClick(R.id.textSignUp)
+    public void StartSignUp(){
+        Intent intent = new Intent(this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
